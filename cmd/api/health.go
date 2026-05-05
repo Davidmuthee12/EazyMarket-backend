6c1e4eccbf813a8120394d848778c7dedad44406
unknown <mutheedavid12@gmail.com>
2026-05-05 15:52:58 +0300

    Add health check handler

--- cmd/api/health.go
+++ cmd/api/health.go
@@ -1,1 +1,25 @@
 package main
+
+import "net/http"
+
+// HealthCheck godoc
+//
+//	@Summary		Health check
+//	@Description	Returns API status, environment and version
+//	@Tags			health
+//	@Accept			json
+//	@Produce		json
+//	@Success		200	{object}	map[string]string
+//	@Failure		500	{object}	error
+//	@Router			/health [get]
+func (app *application) healthCheckHandler(w http.ResponseWriter, r *http.Request) {
+	data := map[string]string{
+		"status":  "ok",
+		"env":     app.config.env,
+		"version": version,
+	}
+
+	if err := app.jsonResponse(w, http.StatusOK, data); err != nil {
+		app.internalServerError(w, r, err)
+	}
+}
