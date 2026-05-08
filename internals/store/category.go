32038ce0397ed8dde52762c029ad5ac9dffc97ba
unknown <mutheedavid12@gmail.com>
2026-05-08 19:41:56 +0300

    feat(store): add GetCategories, DeleteCategory, and UpdateCategory store methods

--- internals/store/category.go
+++ internals/store/category.go
@@ -66,3 +66,102 @@ func (s *CategoryStore) AddCategory(ctx context.Context, category *Category) err
 
 	return nil
 }
+
+func (s *CategoryStore) GetCategories(ctx context.Context) ([]Category, error) {
+	query := `
+		SELECT
+			id,
+			name,
+			slug,
+			image_url,
+			created_at
+		FROM categories
+	`
+
+	ctx, cancel := context.WithTimeout(ctx, QueryTimeoutDuration)
+	defer cancel()
+
+	rows, err := s.db.QueryContext(ctx, query)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	categories := []Category{}
+
+	for rows.Next() {
+		var category Category
+
+		err := rows.Scan(
+			&category.ID,
+			&category.Name,
+			&category.Slug,
+			&category.Image_URL,
+			&category.CreatedAt,
+		)
+
+		if err != nil {
+			return nil, err
+		}
+
+		categories = append(categories, category)
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+
+	if len(categories) == 0 {
+		return nil, ErrNotFound
+	}
+
+	return categories, nil
+}
+
+func (s *CategoryStore) DeleteCategory(ctx context.Context, categoryID string) error {
+	query := `
+		DELETE FROM categories
+		WHERE id = $1
+	`
+
+	ctx, cancel := context.WithTimeout(ctx, QueryTimeoutDuration)
+	defer cancel()
+
+	_, err := s.db.ExecContext(
+		ctx,
+		query,
+		categoryID,
+	)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (s *CategoryStore) UpdateCategory(ctx context.Context, category *Category) error {
+	query := `
+		UPDATE categories
+		SET name = $1, slug = $2, image_url = $3
+		WHERE id = $4
+	`
+
+	ctx, cancel := context.WithTimeout(ctx, QueryTimeoutDuration)
+	defer cancel()
+
+	_, err := s.db.ExecContext(
+		ctx,
+		query,
+		category.Name,
+		category.Slug,
+		category.Image_URL,
+		category.ID,
+	)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
