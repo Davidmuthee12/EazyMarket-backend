52d38bfd557e46b74b80d216068649fbdc1d0a34
unknown <mutheedavid12@gmail.com>
2026-05-09 17:28:36 +0300

    feat(products): handle vendor product listing

--- cmd/api/products.go
+++ cmd/api/products.go
@@ -7,15 +7,15 @@ import (
 )
 
 type ProductPayload struct {
-	Name           string `json:"name" validate:"required,max=100"`
-	Slug           string `json:"slug" validate:"required,max=100"`
-	Description    string `json:"description" validate:"required,max=250"`
-	Category_ID    string `json:"category_id" validate:"omitempty,uuid"`
-	Price          int64  `json:"price" validate:"required"`
-	Compare_Price  int64  `json:"compare_price" validate:"omitempty"`
-	Stock_Quantity int64  `json:"stock_quantity" validate:"required"`
-	SKU            string `json:"sku" validate:"required,max=50"`
-	Weight         int64  `json:"weight" validate:"omitempty"`
+	Name           string  `json:"name" validate:"required,max=100"`
+	Slug           string  `json:"slug" validate:"required,max=100"`
+	Description    string  `json:"description" validate:"required,max=250"`
+	Category_ID    string  `json:"category_id" validate:"omitempty,uuid"`
+	Price          float64 `json:"price" validate:"required"`
+	Compare_Price  float64 `json:"compare_price" validate:"omitempty"`
+	Stock_Quantity int     `json:"stock_quantity" validate:"required"`
+	SKU            string  `json:"sku" validate:"required,max=50"`
+	Weight         float64 `json:"weight" validate:"omitempty"`
 }
 
 // CreateProduct godoc
@@ -83,4 +83,41 @@ func (app *application) postProductsHandler(w http.ResponseWriter, r *http.Reque
 		app.internalServerError(w, r, err)
 	}
 
+}
+
+// GetAllProducts godoc
+//
+//	@Summary		Get all products
+//	@Description	Retrieves all products for the authenticated vendor.
+//	@Tags			products
+//	@Produce		json
+//	@Success		200	{array}		store.Products	"Products retrieved"
+//	@Failure		400	{object}	error
+//	@Failure		500	{object}	error
+//	@Security		ApiKeyAuth
+//	@Router			/vendor/products [get]
+func (app *application) getAllProducts(w http.ResponseWriter, r *http.Request) {
+	vendor := getUserFromCtx(r)
+	if vendor == nil {
+		app.internalServerError(w, r, nil)
+		return
+	}
+
+	ctx := r.Context()
+
+	products, err := app.store.Product.GetAllProduct(ctx, vendor.UUID)
+	if err != nil {
+		switch err {
+		case store.ErrNotFound:
+			app.badRequestResponse(w, r, err)
+			return
+		default:
+			app.internalServerError(w, r, err)
+			return
+		}
+	}
+
+	if err := app.jsonResponse(w, http.StatusOK, products); err != nil {
+		app.internalServerError(w, r, err)
+	}
 }
